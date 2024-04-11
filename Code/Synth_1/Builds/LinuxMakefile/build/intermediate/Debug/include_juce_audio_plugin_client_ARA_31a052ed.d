3dd399ce734c35a9c06bbbaa3c4f58483d0fc718
Yassine Saheb Ettabaa <50518578+YassineSE@users.noreply.github.com>
2024-04-11 17:25:21 +0200

    Delete Code/Synth_1/Builds directory

--- Code/Synth_1/Builds/LinuxMakefile/build/intermediate/Debug/include_juce_audio_plugin_client_ARA_31a052ed.d
+++
@@ -1,6 +0,0 @@
-build/intermediate/Debug/include_juce_audio_plugin_client_ARA_31a052ed.o: \
- ../../JuceLibraryCode/include_juce_audio_plugin_client_ARA.cpp \
- /home/yassine/Documents/JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client_ARA.cpp \
- /home/yassine/Documents/JUCE/modules/juce_audio_plugin_client/ARA/juce_ARA_Wrapper.cpp \
- /home/yassine/Documents/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
- /home/yassine/Documents/JUCE/modules/juce_audio_plugin_client/ARA/../utility/juce_CheckSettingMacros.h
--- Code/Synth_1/Builds/LinuxMakefile/build/intermediate/Debug/include_juce_audio_processors_ara_2a4c6ef7.d
+++
@@ -1,7 +0,0 @@
-build/intermediate/Debug/include_juce_audio_processors_ara_2a4c6ef7.o: \
- ../../JuceLibraryCode/include_juce_audio_processors_ara.cpp \
- /home/yassine/Documents/JUCE/modules/juce_audio_processors/juce_audio_processors_ara.cpp \
- /home/yassine/Documents/JUCE/modules/juce_core/system/juce_CompilerWarnings.h \
- /home/yassine/Documents/JUCE/modules/juce_core/system/juce_TargetPlatform.h \
- /home/yassine/Documents/JUCE/modules/juce_audio_processors/utilities/ARA/juce_ARADebug.h \
- /home/yassine/Documents/JUCE/modules/juce_core/system/juce_PlatformDefs.h
